2c0540287b0a526759aacff56985307717baec0e
xche529 <126313171+xche529@users.noreply.github.com>
2023-07-06 13:37:32 +0800

    Added image process script to rescale image

---
+++ src/ImageProcessScript.py
@@ -0,0 +1,31 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+import os
+
+
+
+row_folder = input("Enter the path to the folder containing the raw images: ")
+target_number = input("Enter the number these images are of: ")
+target_folder = input("Enter the path to the folder you want to save the processed images to: ")
+next_suffix = 0
+
+for filename in os.listdir(row_folder):
+    # Check if the file is an image (JPEG, PNG, etc.)
+    if filename.endswith('.png'):
+        # Construct the complete file path
+        file_path = os.path.join(row_folder, filename)
+        # Open the image
+        image = Image.open(file_path)
+        resized_image = image.resize((500, 500))
+        next_suffix += 1
+        filename = f'{target_number}_processed_image_{next_suffix}.png'
+        save_path = os.path.join(target_folder, filename)
+        resized_image.save(save_path)
+        image.close()
+        resized_image.close()
+
+        
+
+#plt.imshow(resized_image)
+#plt.show()
+
--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -1,4 +1,5 @@
 import numpy
+import scipy.special
 
 class neuralNetwork:
     
@@ -10,10 +11,16 @@ class neuralNetwork:
         # create weight matrices, wih and who with random normal distribution 
         self.wih = numpy.random.normal(0.0, pow(self.Hid, -0.5), (self.Hid, self.In))
         self.who = numpy.random.normal(0.0, pow(self.Out, -0.5), (self.Out, self.Hid))
-        
+        # define activation function 
+        self.activation_function = lambda x: scipy.special.expit(x)
         
     def train() :
         pass
         
-    def query() :
-        pass
+    def query(self,input_list):
+        inputs = numpy.array(input_list, ndmin=2).T
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+        

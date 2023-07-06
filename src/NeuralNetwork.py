20f029d4e863fdfe95a4e1c6a9347a444a812dbe
xche529 <126313171+xche529@users.noreply.github.com>
2023-07-06 11:30:36 +0800

    finished init method

--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -1,8 +1,17 @@
+import numpy
+
 class neuralNetwork:
     
-    def __init__() :
-        pass
-    
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate) :
+        self.In = inputnodes
+        self.Hid = hiddennodes
+        self.Out = outputnodes
+        self.Lr = learningrate
+        # create weight matrices, wih and who with random normal distribution 
+        self.wih = numpy.random.normal(0.0, pow(self.Hid, -0.5), (self.Hid, self.In))
+        self.who = numpy.random.normal(0.0, pow(self.Out, -0.5), (self.Out, self.Hid))
+        
+        
     def train() :
         pass
         
---
+++ src/main.py
@@ -0,0 +1,7 @@
+from NeuralNetwork import neuralNetwork
+
+input = 3
+output = 3
+hidden = 3
+Lr = 0.3
+n = neuralNetwork(input, hidden, output, Lr)
